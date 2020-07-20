12f89f0b8844313d20c21f5f9a971f3cbc6463d5
kaizhou.li <kaizhou.li@seetatech.com>
2020-07-20 19:28:32 +0800

    add hello world.

---
+++ include/ohm/hello.h
@@ -0,0 +1,16 @@
+//
+// Created by kier on 2020/7/20.
+//
+
+#ifndef OMEGA_HELLO_H
+#define OMEGA_HELLO_H
+
+#include <iostream>
+
+namespace ohm {
+    inline void hello() {
+        std::cout << "Hey, I'm running!" << std::endl;
+    }
+}
+
+#endif //OMEGA_HELLO_H
---
+++ test/hello.cpp
@@ -0,0 +1,11 @@
+//
+// Created by kier on 2020/7/20.
+//
+
+#include "ohm/hello"
+
+int main() {
+    ohm::hello();
+    return 0;
+}
+
