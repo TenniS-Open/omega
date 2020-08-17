8b191fd1cbe8c33f504a4bfd93c617739de914cb
kaizhou.li <kaizhou.li@seetatech.com>
2020-08-18 01:01:30 +0800

    now add assert, assert result can be write to logger.

---
+++ ctest/assert.cpp
@@ -0,0 +1,37 @@
+//
+// Created by kier on 2020/7/31.
+//
+
+#include "ohm/assert.h"
+#include "ohm/logger.h"
+
+#define N(n) (n)
+
+int main() {
+    try {
+        // assert with empty message
+        ohm_assert(2 == N(1), "");
+    } catch (...) {
+        ohm::println("(2 == N(1)) [Check]");
+    }
+
+    try {
+        // assert with given message
+        ohm_assert(2 == N(1), "2 must equal N(1).");
+    } catch (...) {
+        ohm::println("(2 == N(1)) [Check]");
+    }
+
+    ohm::Logger log;
+
+    try {
+        // assert with logger, write to log if failed.
+        ohm_assert(3 == N(2), log, "3 must equal 2.");
+        ohm::print("AAA");
+    } catch (...) {
+        ohm::println("(3 == N(2)) [Check]");
+    }
+
+    return 0;
+}
+
--- include/ohm/assert.h
+++ include/ohm/assert.h
@@ -8,13 +8,43 @@
 #include "print.h"
 #include "logger.h"
 
+namespace ohm {
+    /**
+     * This print is work for assert, incase auto generate never achive codes.
+     * well, now version assert compile speed very slow. OMG.
+     */
+    template<>
+    struct printable<Logger> {
+        using type = Logger;
+
+        void print(std::ostream &out, const type &x) {}
+    };
+
+    template <typename T>
+    auto __forward_assert_logger(T &&t) -> decltype(std::forward<T>(t)) {
+        return std::forward<T>(t);
+    }
+
+    auto __forward_assert_logger(const Logger &logger) -> LogStream {
+        return logger(LOG_ERROR);
+    }
+
+    auto __forward_assert_logger(Logger &logger) -> LogStream {
+        return logger(LOG_ERROR);
+    }
+}
+
 #define ohm_assert(cond, log, ...) \
-    if (!(cond)) do {\
-        ohm::println(log, "Assertion failed: ", "(" #cond ")", \
-            ", function ", __func__, \
-            ", file ", __FILE__, \
-            ", line ", __LINE__, \
-            ".", ## __VA_ARGS__); \
+    if (!(cond)) do { \
+        auto constexpr ohm_auto_name(__is_logger) = \
+                std::is_convertible<decltype(log), const ohm::Logger&>::value; \
+        if (ohm_auto_name(__is_logger)) { \
+            ohm_log(ohm::__forward_assert_logger(log), "Assertion failed: ", "(" #cond ")", \
+            ". ", ## __VA_ARGS__); \
+        } else { \
+            ohm_log(ohm::LOG_ERROR, "Assertion failed: ", "(" #cond ")", \
+            ". ", log, ## __VA_ARGS__); \
+        } \
     } while (false)
 
 #endif //OMEGA_ASSERT_H
